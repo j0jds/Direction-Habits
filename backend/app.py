bf02ec3444ea01ecbab7a37c3665c04ce87833e2
j0jds <mrlucassilva7342@gmail.com>
2024-07-04 15:27:28 -0300

    Defined pages routes of website

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route("/") 
 def home():
-    return "Hello world!"
+    
+    return render_template("home.html")
 
-app.run()
+@app.route("/cadastro")
+def cadastro():
+    
+    return render_template("cadastro.html")
+
+@app.route("/login")
+def login():
+    
+    return render_template("login.html")
+
+@app.route("/tarefas")
+def tarefas():
+    
+    return render_template("tarefas.html")
+
+@app.route("/novatarefa")
+def novatarefa():
+    
+    return render_template("novatarefa.html")
+
+@app.route("/minhastarefas")
+def minhastarefas():
+    
+    return render_template("minhastarefas.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
