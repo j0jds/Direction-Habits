15de7ce5f38b12e31bac624e2c3ed5c177286633
j0jds <mrlucassilva7342@gmail.com>
2024-07-08 20:07:50 -0300

    Exhibition of description in tasks

---
+++ backend/static/js/minhastarefas.js
@@ -0,0 +1,8 @@
+function toggleDescription(taskId) {
+    var descriptionElement = document.getElementById('descricao-' + taskId);
+    if (descriptionElement.style.display === 'none') {
+        descriptionElement.style.display = 'block';
+    } else {
+        descriptionElement.style.display = 'none';
+    }
+}
